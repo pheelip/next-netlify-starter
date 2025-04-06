8e3df65806734b915248954e4864840ce51b175c
pheelip <pheelip.sim@gmail.com>
2025-04-06 14:58:33 +0800

    Update get-question.js

--- pages/functions/get-question.js
+++ pages/functions/get-question.js
@@ -1,1 +1,17 @@
-
+// functions/get-question.js
+exports.handler = async (event) => {
+    try {
+        const response = await fetch('YOUR_N8N_QUESTION_WEBHOOK_URL');
+        const data = await response.json();
+        
+        return {
+            statusCode: 200,
+            body: JSON.stringify(data)
+        };
+    } catch (error) {
+        return {
+            statusCode: 500,
+            body: JSON.stringify({ error: "Failed to fetch question" })
+        };
+    }
+};
